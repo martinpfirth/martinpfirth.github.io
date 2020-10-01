5f5fab816d6acef53332788d7d55457a7a8ae44a
Martin Firth <micomator@gmail.com>
2020-10-01 23:25:26 +0800

    Remove some old commented out code

--- index.php
+++ index.php
@@ -12,11 +12,9 @@
     <title>Watch Calendar</title>
 </head>
 <body>
-
     <div id="header">  
         <h1>Watch Calendar</h1>
     </div>
-    
     <div class="container">
             <form autocomplete="off" id="setup">
                 I want to watch
@@ -36,7 +34,6 @@
                         <option value="weekday">week day</option>
                         <option value="weekendday">weekend day</option>
                         <option value="week">week</option>
-                        <!-- <option value="month">month</option> -->
                     </select>
                 </div>
                 <br />
@@ -78,40 +75,9 @@
                 </div>
 
                 <button id="create_schedule">Create Schedule <img src="magic-wand.svg" alt="wand icon" /></button>
-                
+
                 <div id="info">Hello</div>
             </form>
-            <!-- If you want to watch seasons 1 - 5 of Better Call Saul finishing on 02/11/2021
-            watching 1 episode every day
-            you will need to start on 28/07/2021 -->
-
-            <!-- You will need to start watching on 28/07/2021 to finish
-            Better Call Saul by 02/11/2021
-            if you're going to watch 1 episode per day. -->
-
-            <!-- Watching 1 episode per day, you'll finish
-            Better Call Saul on 02/11/2021 if you start on
-            28/07/2021. -->
-            
-            <!-- 
-            <p>
-            Start watching Season 2 of Better Call on 28/07/2021
-            and you will finish on 02/11/2021
-            if you watch 1 episode per day.
-            </p>
-
-            <p>
-                That's 89 episodes and 2928 minutes total! Episodes are 44 minutes in length on average.
-            </p>
-
-            Seasons:
-            Episodes:
-            Avergae Episode length:
-            Total Watch time:
-            Start date:
-            End date:
-            
-            -->
             <div id="calendar"></div>
     </div> <!-- container -->
 </body>
